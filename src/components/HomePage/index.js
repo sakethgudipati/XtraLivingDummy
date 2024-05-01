8d3e9bd595f774fbb9e8c22efd1b9c8ffc8610ea
sakethgudipati <sakethkasyap.gudipati@gmail.com>
2024-05-01 16:01:42 +0530

    XtraLivingDummy

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,42 @@
+import "./index.css"
+
+const HomePage = () => (
+    <>
+        <div className="home-bg-container">
+            <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714210279/Menu_jupmrk.png" className="nav-icon" alt="nav icon" />
+            <div className="main-heading-container">
+                <h1 className="x60-main-heading">X60 OUTDOOR <br/><span className="x60-sub-heading">Start Your Fitness Journey With Nature!</span></h1>
+            </div>
+            <div>
+                <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714376907/Vector_4_aoojqn.png" className="line" alt="line" />
+                <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714376907/Vector_18_xp08nz.png" className="arrow" alt="arrow" />
+                <div className="group-container">
+                    <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714548170/Group_34139_jtouqq.png" alt="group" className="group1" />
+                    <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714548170/Group_34140_gqsivw.png" alt="group" className="group2" />
+                </div>
+            </div>
+            <div className="part2-container">
+                    <div className="part2-elements">
+                        <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714535693/Vector_17_yfmsd0.png" className="arrow2" alt="arrow" />
+                        <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714535694/Group_896_zpye0v.png" className="whatsapp-icon" alt="whatsapp" />
+                        <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714541870/Group_178_vebkbn.png" className="dot-icon" alt="dots" />
+                    </div>
+                    <div className="x60-info-container">
+                        <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714535693/Rectangle_175_wa0byj.png" className="outline" alt="outline" />
+                        <div className="info">
+                            <p className="info-para">Forged from evolution, neuroscience, and math, our mission is to elevate human potential. Beyond products, we're shaping a future where every family thrives in <span className="info-para-highlight">holistic health</span> and <span className="info-para-highlight">peak performance</span>.</p>
+                        </div>
+                    </div>
+            </div>
+            <div className="part3-container">
+                    <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714556879/Vector_5_oueeww.png" alt="line" className="line2" />
+                    <div className="part3-info-container">
+                        <p className="part3-info">At <span className="part3-info-highlight">X60</span>, we believe in the power of nature and its profound impact on our health. Our fitness studio is nestled outdoors, encouraging synchronization with the environment as you train. We focus on functional movements that promote strength, flexibility, and balance, enhancing your health in the most natural setting. Alongside our fitness philosophy, we emphasize the importance of nutrition from real food, guiding you to nourish your body with the best that nature offers.</p>
+                        <img src="https://res.cloudinary.com/defacaof3/image/upload/v1714557366/Group_912_xnceve.png" alt="group" className="group3" />
+                    </div>
+            </div>
+        </div>
+    </>
+)
+
+export default HomePage
